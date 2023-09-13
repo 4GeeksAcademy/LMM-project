6da41cd47c4b5cd2d8d96885aa1eec55bfb7a62e
LuisPereira05 <144857040+LuisPereira05@users.noreply.github.com>
2023-09-13 00:30:07 +0000

    Añade bien

--- main/main.py
+++ main/main.py
@@ -1,4 +1,13 @@
 
+def updateDatabase(userName, password):
 
-with open("/workspaces/LMM-project/data.txt", "w") as file:
-    file.write("wasdds")
+    with open("/workspaces/LMM-project/data.txt", "a") as file:
+        #appendedStr = str("user(", userName, ",", password, ")\n")
+        file.write("user("+ userName + "," + password + ")\n")
+
+dataDict = {}
+
+us = input("Enter Username: ")
+pas = input("Enter Password: ")
+
+updateDatabase(us, pas)
